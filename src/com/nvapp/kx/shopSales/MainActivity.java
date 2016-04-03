3d0a6747f61164b41642817f1bd288e47391e245
lexloo <lexloo@126.com>
2016-04-03 22:57:43 +0800

    splashWindow

--- src/com/nvapp/kx/shopSales/MainActivity.java
+++ src/com/nvapp/kx/shopSales/MainActivity.java
@@ -1,15 +1,59 @@
 package com.nvapp.kx.shopSales;
 
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 		setContentView(R.layout.activity_main);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			setTranslucentStatus(true);
+//			ViewGroup contentView = (ViewGroup) this.findViewById(android.R.id.content);
+//			View statusBarView = new View(this);
+//			ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+//					getStatusBarHeight(this));
+//			statusBarView.setBackgroundResource(R.color.red);
+//			contentView.addView(statusBarView, lp);
+		}
+		//
+		// SystemBarTintManager tintManager = new SystemBarTintManager(this);
+		// tintManager.setStatusBarTintEnabled(true);
+		// tintManager.setStatusBarTintResource(R.color.red);
+	}
+
+	public static int getStatusBarHeight(Context context) {
+		int result = 0;
+		int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+		if (resourceId > 0) {
+			result = context.getResources().getDimensionPixelSize(resourceId);
+		}
+		return result;
+	}
+
+	@TargetApi(19)
+	private void setTranslucentStatus(boolean on) {
+		Window win = getWindow();
+		WindowManager.LayoutParams winParams = win.getAttributes();
+		final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+		if (on) {
+			winParams.flags |= bits;
+		} else {
+			winParams.flags &= ~bits;
+		}
+		win.setAttributes(winParams);
 	}
 }
